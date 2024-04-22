3316b98f0979b740be9da3c16296465ec4a9235c
pedro1lopezpena <157753468+pedro1lopezpena@users.noreply.github.com>
2024-04-22 11:49:36 -0500

    Update shoppingCart.cpp
    some more code added

--- shoppingCart.cpp
+++ shoppingCart.cpp
@@ -17,3 +17,33 @@ string ShoppingCart::GetCustomerName() const {
   return customerName;
 }
   
+string ShoppingCart::GetDate() const {
+  return currentDate;
+}
+
+void ShoppingCart::AddItem(ItemToPurchase item){
+  cartItems.push_back(item);
+}
+
+void ShoppingCart::RemoveItem(string name){
+  bool itemExists = 0;
+
+  for (unsigned int i=0; i < cartItems.size(); i++){
+    if (cartItems.at(i).GetName() = name){
+        cartItems.erase(cartItems.begin() + i);
+        itemExists = 1
+          }
+  }
+  if (itemExists == 0){
+      cout << 'Item not found in cart. Nothing removed." << endl;
+        }
+}
+
+int ShoppingCart::GetQuantity() const {
+  return quantity;
+}
+
+void ShoppingCart::SetQuantity(int quantity){
+  quantity = this->quantity;
+
+void ShoppingCart::ModifyItem(string name, int quantity
