dac62d54ad04c8ac08373ee80bf2bf94450fdf89
pedro1lopezpena <157753468+pedro1lopezpena@users.noreply.github.com>
2024-04-22 16:04:13 -0500

    Update shoppingCart.cpp

--- shoppingCart.cpp
+++ shoppingCart.cpp
@@ -8,9 +8,9 @@ ShoppingCart::ShoppingCart(){
   currentDate = "January 1, 2024";
 }
   
-ShoppingCart::ShoppingCart(string name, string date){
-  customerName = name;
-  currentDate = date;
+ShoppingCart::ShoppingCart(string t_name, string t_date){
+  customerName = t_name;
+  currentDate = t_date;
 }
 
 string ShoppingCart::GetCustomerName() const {
@@ -25,11 +25,11 @@ void ShoppingCart::AddItem(ItemToPurchase item){
   cartItems.push_back(item);
 }
 
-void ShoppingCart::RemoveItem(string name){
+void ShoppingCart::RemoveItem(string t_name){
   bool itemExists = 0;
 
   for (unsigned int i=0; i < cartItems.size(); i++){
-    if (cartItems.at(i).GetName() = name){
+    if (cartItems.at(i).GetName() = t_name){
         cartItems.erase(cartItems.begin() + i);
         itemExists = 1
           }
@@ -43,7 +43,48 @@ int ShoppingCart::GetQuantity() const {
   return quantity;
 }
 
-void ShoppingCart::SetQuantity(int quantity){
-  quantity = this->quantity;
+void ShoppingCart::SetQuantity(int t_quantity){
+  quantity = t_quantity;
 
-void ShoppingCart::ModifyItem(string name, int quantity
+void ShoppingCart::ModifyItem(string t_name, int t_quantity) {
+
+    bool itemExists = 0;
+
+    for (unsigned int i = 0; i < cartItems.size(); i++) {
+        if (cartItems.at(i).GetName() == t_name) {
+            itemExists = 1;
+            cartItems.at(i).SetQuantity(t_quantity);
+        }
+    }
+
+    if (itemExists == 0) {
+        cout << "Item not found in cart. Nothing modified." << endl;
+    }
+}
+
+int ShoppingCart::GetNumItemsInCart() const {
+    int numItems = 0;
+
+    for (unsigned int i = 0; i < cartItems.size(); i++) {
+        numItems += cartItems[i].GetQuantity();
+    }
+
+    return numItems;
+}
+
+int ShoppingCart::GetCostOfCart() const {
+    int totalCost = 0;
+    for (unsigned int i = 0; i < cartItems.size(); i++) {
+        totalCost += cartItems[i].GetQuantity() * cartItems[i].GetPrice();
+    }
+
+    return totalCost;
+}
+
+string ShoppingCart::GetItemName() const {
+    return itemName;
+}
+
+int ShoppingCart::GetPrice() const {
+    return price;
+}
