206028b4b7eb1a5e28a777bb64ac10687069225d
pedro1lopezpena <157753468+pedro1lopezpena@users.noreply.github.com>
2024-04-30 11:05:50 -0500

    Update shoppingCart.cpp
    last updates

--- shoppingCart.cpp
+++ shoppingCart.cpp
@@ -1,124 +1,78 @@
 // William Miller and Pedro Lopez
-//
 
-#include  "ShoppingCart.h"
+#include "ItemToPurchase.h"
+#include <iostream>
 
-ShoppingCart::ShoppingCart(){
-  customerName = "none";
-  currentDate = "January 1, 2024";
-}
-  
-ShoppingCart::ShoppingCart(string t_name, string t_date){
-  customerName = t_name;
-  currentDate = t_date;
-}
+ 
+ItemToPurchase::ItemToPurchase(const string& name, const string& description, int price, int quantity)
 
-string ShoppingCart::GetCustomerName() const {
-  return customerName;
-}
-  
-string ShoppingCart::GetDate() const {
-  return currentDate;
+    : itemName(name), itemDescription(description), itemPrice(price), itemQuantity(quantity) {}
+ 
+
+void ItemToPurchase::SetName(const string& name) {
+
+    itemName = name;
+
 }
 
-void ShoppingCart::AddItem(ItemToPurchase item){
-  cartItems.push_back(item);
-}
 
-void ShoppingCart::RemoveItem(string t_name){
-  bool itemExists = 0;
+string ItemToPurchase::GetName() const {
 
-  for (unsigned int i=0; i < cartItems.size(); i++){
-    if (cartItems.at(i).GetName() = t_name){
-        cartItems.erase(cartItems.begin() + i);
-        itemExists = 1
-          }
-  }
-  if (itemExists == 0){
-      cout << 'Item not found in cart. Nothing removed." << endl;
-        }
-}
-
-int ShoppingCart::GetQuantity() const {
-  return quantity;
-}
-
-void ShoppingCart::SetQuantity(int t_quantity){
-  quantity = t_quantity;
-
-void ShoppingCart::ModifyItem(string t_name, int t_quantity) {
-
-    bool itemExists = 0;
-
-    for (unsigned int i = 0; i < cartItems.size(); i++) {
-        if (cartItems.at(i).GetName() == t_name) {
-            itemExists = 1;
-            cartItems.at(i).SetQuantity(t_quantity);
-        }
-    }
-
-    if (itemExists == 0) {
-        cout << "Item not found in cart. Nothing modified." << endl;
-    }
-}
-
-int ShoppingCart::GetNumItemsInCart() const {
-    int numItems = 0;
-
-    for (unsigned int i = 0; i < cartItems.size(); i++) {
-        numItems += cartItems[i].GetQuantity();
-    }
-
-    return numItems;
-}
-
-int ShoppingCart::GetCostOfCart() const {
-    int totalCost = 0;
-    for (unsigned int i = 0; i < cartItems.size(); i++) {
-        totalCost += cartItems[i].GetQuantity() * cartItems[i].GetPrice();
-    }
-
-    return totalCost;
-}
-
-string ShoppingCart::GetItemName() const {
     return itemName;
+
 }
 
-int ShoppingCart::GetPrice() const {
-    return price;
+ 
+void ItemToPurchase::SetPrice(int price) {
+
+    itemPrice = price;
+
 }
 
-void ShoppingCart::PrintTotal() {
+int ItemToPurchase::GetPrice() const {
 
-    int total = 0;
+    return itemPrice;
 
-    if (cartItems.size() == 0) {
-        cout << "SHOPPING CART IS EMPTY" << endl << endl;
-        cout << "Total: $" << total << endl << endl;
-    }
-    else {
-        for (unsigned int i = 0; i < cartItems.size(); i++) {
-            cout << cartItems[i].GetName() << " " << cartItems[i].GetQuantity() << " @ $" << cartItems[i].GetPrice();
-            cout << " = $" << (cartItems[i].GetQuantity() * cartItems[i].GetPrice()) << endl;
-        }
-
-        for (unsigned int k = 0; k < cartItems.size(); k++) {
-            total += (cartItems[k].GetQuantity() * cartItems[k].GetPrice());
-        }
-
-        cout << endl;
-        cout << "Total: $" << total << endl << endl;
-    }
 }
 
-string ShoppingCart::GetDescription() const {
-    return description;
+
+void ItemToPurchase::SetQuantity(int quantity) {
+
+    itemQuantity = quantity;
+
+}
+ 
+
+int ItemToPurchase::GetQuantity() const {
+
+    return itemQuantity;
+
 }
 
-void ShoppingCart::PrintDescriptions() {
-    for (unsigned int i = 0; i < cartItems.size(); i++) {
-        cout << cartItems[i].GetName() << ": " << cartItems[i].GetDescription() << endl;
-    }
-    cout << endl;
+
+void ItemToPurchase::SetDescription(const string& description) {
+
+    itemDescription = description;
+
+}
+
+
+string ItemToPurchase::GetDescription() const {
+
+    return itemDescription;
+
+}
+ 
+
+void ItemToPurchase::PrintItemCost() const {
+
+    cout << itemName << " " << itemQuantity << " @ $" << itemPrice << " = $" << (itemQuantity * itemPrice) << endl;
+
+}
+
+ 
+void ItemToPurchase::PrintItemDescription() const {
+
+    cout << itemName << ": " << itemDescription << endl;
+
 }
